03602722f11b4d5d0c09efa2c3704080112da038
Joseph O'Brien <jmobrien127@gmail.com>
2025-02-13 03:32:09 -0700

    move pdf

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -9,18 +9,14 @@ const Resume: React.FC = () => {
       </div>
 
       <iframe
-        src="/assets/jmo_cv_se.pdf"
+        src="/jmo_cv_se.pdf"
         width="100%"
         height="1000px"
         title="Joseph O'Brien CV"
       />
       <p>
         If the PDF does not display, you can download it{' '}
-        <a
-          href="/assets/jmo_cv_se.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="/jmo_cv_se.pdf" target="_blank" rel="noopener noreferrer">
           here
         </a>
         .
