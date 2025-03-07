31cb72778497363792701119ff4a276e4de458ff
Joseph O'Brien <jmobrien127@gmail.com>
2025-03-07 16:53:47 -0700

    feat: add smooth background image loading transition

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import TabBar from './TabBar';
 import Footer from './Footer';
 import { LayoutProps } from '../../types';
 
-const LayoutWrapper = styled.div`
+interface WrapperProps {
+  isLoaded: boolean;
+}
+
+const LayoutWrapper = styled.div<WrapperProps>`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
@@ -13,6 +17,20 @@ const LayoutWrapper = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-attachment: fixed;
+  opacity: ${(props) => (props.isLoaded ? 1 : 0)};
+  transition: opacity 0.3s ease-in;
+
+  &::before {
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    z-index: 0;
+    pointer-events: none;
+  }
 `;
 
 const MainContent = styled.main`
@@ -26,8 +44,16 @@ const MainContent = styled.main`
 `;
 
 const MainLayout: React.FC<LayoutProps> = ({ children }) => {
+  const [isBackgroundLoaded, setIsBackgroundLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = '/assets/bg_photos/bg5.png';
+    img.onload = () => setIsBackgroundLoaded(true);
+  }, []);
+
   return (
-    <LayoutWrapper>
+    <LayoutWrapper isLoaded={isBackgroundLoaded}>
       <TabBar />
       <MainContent>{children}</MainContent>
       <Footer />
