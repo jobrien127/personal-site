5e91cd9e4f2d1199fd1bc79273ca57531e6d2cef
Joseph O'Brien <dev@jm-ob.com>
2025-03-25 01:31:11 -0600

    feat: add new project

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -13,39 +13,46 @@ interface Project {
 const projects: Project[] = [
   {
     id: 1,
-    title: 'Pantry iOS App (Work in Progress)',
+    title: 'Pantry iOS App (WIP)',
     description:
-      'A modern iOS app for smart pantry management, helping to track groceries, manage expiration dates, and receive intelligent suggestions based on purchasing habits. (Not on App Store yet)',
+      'A modern iOS app for smart pantry management, helping to track groceries, manage expiration dates, and receive intelligent suggestions based on purchasing habits.',
     link: 'https://github.com/jobrien127/pantry',
   },
   {
     id: 2,
+    title: 'HoodViz - Robinhood Portfolio Visualizer',
+    description:
+      'A python script that provides visualizations of a Robinhood portfolio.',
+    link: 'https://github.com/jobrien127/hoodviz',
+  },
+  {
+    id: 3,
     title: 'Personal Website',
     description: 'Repository for this website.',
     link: 'https://github.com/jobrien127/personal-site',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Python Playground',
     description:
       'A python playground for experiementing with machine learning and AI implementations.',
     link: 'https://github.com/jobrien127/python-playground',
   },
   {
-    id: 4,
-    title: 'Tempo (Work in Progress)',
+    id: 5,
+    title: 'Tempo',
     description:
       'A Templated repo that is meant to help kickstart containerized full-stack development. Makes use of Docker, React, React-Native, Django, Go, postgreSQL, MongoDB, Redis, and RabbitMQ.',
     link: 'https://github.com/jobrien127/tempo',
   },
   {
-    id: 5,
+    id: 6,
     title: '30XX Arena Survival',
     description: 'A 2D game created with GameMaker Studio.',
     link: 'https://github.com/jobrien127/30XX_Arena_Survival',
   },
   {
-    id: 6,
+    id: 7,
     title: 'lemmonlodge.com',
     description:
       'Website I built for an idle domain, which sends automated emails to domain owner if site visitor wants to contact them.',
@@ -53,14 +60,14 @@ const projects: Project[] = [
     sitelink: 'https://www.lemmonlodge.com',
   },
   {
-    id: 7,
+    id: 8,
     title: 'Zodiac Sign Calculator',
     description:
       'Ruby program that calculates your zodiac sign based on your birthdate.',
     link: 'https://github.com/jobrien127/zodiac-sign-calculator',
   },
   {
-    id: 8,
+    id: 9,
     title: 'Markov Chain Text Generator',
     description:
       'Ruby program that generates text based on a given input text file.',
